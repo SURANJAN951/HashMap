c4245663a201e3db96c3f06401b8be56a6822eca
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-21 23:32:07 +0530

    HashMapExample

---
+++ HashMapExample.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap hm1=new HashMap();
+        hm1.put(01,"virat");//Entry
+        hm1.put(02,"Rohit");
+        hm1.put(03,"Rohan");
+        hm1.put(01,"virat");
+        hm1.put(null,null);//null
+        System.out.println(hm1);
+    }
+}
